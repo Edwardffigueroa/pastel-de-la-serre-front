4a58220b19ed9d513cdb7ce4dff5b4b5d9e608cc
Edwar Figueroa <edwardfigueroavelasco@gmail.com>
2020-07-30 09:57:11 -0500

    App Base

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Base App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
