9af7176dd5758faed26f6985f27c0d4ca7de1f21
JuanSevillano <sevi0_6@hotmail.com>
2020-08-14 23:36:17 +0200

    adding buttons' rows

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import classes from './Button.module.css'
-import row from '../../../assets/images/nav/row.svg'
-const Button = props => {
 
+import row from '../../../assets/images/nav/row.svg'
+import row2 from '../../../assets/images/nav/row2.svg'
+
+
+
+const Button = props => {
     const myClasses = [classes.Button, props.type].join(' ')
+    let color = window.innerWidth > 500 ? row : row2
 
     return <button
         className={myClasses}
         onClick={props.clicked}>
         {props.children}
-        <img src={row} alt="Go, prochaine" />
+        <img className={classes.Row} src={color} alt="Go, prochaine" />
     </button>
 }
 
