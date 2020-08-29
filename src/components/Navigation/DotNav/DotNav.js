df154aa02d14bfd427942e0847aa23d56adf88f8
JuanSevillano <sevi0_6@hotmail.com>
2020-08-29 21:50:18 +0200

    adding dotnav animations

--- src/components/Navigation/DotNav/DotNav.js
+++ src/components/Navigation/DotNav/DotNav.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import classes from './DotNav.module.css'
-import { useHistory } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const DotNav = () => {
-
-    const history = useHistory()
-    const location = history.location.pathname
-
-    const dotClickHandler = e => {
-        const nextURl = e.target.id
-        const currentActive = document.querySelector(`#${nextURl}`)
-        
-        history.push(`/${nextURl}`)
-    }
-
     return (
         <div className={classes.DotNav}>
-            <div onClick={dotClickHandler} id="notre-histoire" className={classes.Dot}></div>
-            <div onClick={dotClickHandler} id="visitez-nous" className={classes.Dot}></div>
-            <div onClick={dotClickHandler} id="boutique" className={classes.Dot}></div>
+            <NavLink
+                to="/notre-histoire"
+                className={classes.Dot}
+                activeClassName={classes.Active}>
+            </NavLink>
+            <NavLink
+                to="/visitez-nous"
+                className={classes.Dot}
+                activeClassName={classes.Active}>
+            </NavLink>
+            <NavLink
+                to="/boutique"
+                className={classes.Dot}
+                activeClassName={classes.Active}>
+            </NavLink>
         </div>
     );
 }
