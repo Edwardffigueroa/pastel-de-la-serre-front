988e1183bb7da4c9553601eae97b90e6ad2115c5
JuanSevillano <sevi0_6@hotmail.com>
2020-09-09 15:08:33 +0200

    index.html added to repository's root path

---
+++ precache-manifest.e8786920a68ef83317eeef44c2f7334a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8dd223123ca371c14e019d7e1a0cb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc233762771cafeeaaf",
+    "url": "/static/css/main.2042c31e.chunk.css"
+  },
+  {
+    "revision": "f6cea806d7d175d2e432",
+    "url": "/static/js/2.72b15781.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72b15781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc233762771cafeeaaf",
+    "url": "/static/js/main.0cd99bd0.chunk.js"
+  },
+  {
+    "revision": "e96a11b7f670f2b3017f",
+    "url": "/static/js/runtime-main.2f2b7b41.js"
+  },
+  {
+    "revision": "11637d2fc0aeb9cd014a41ea95c21a4f",
+    "url": "/static/media/Aerial view of Bojnice medieval castle, UNESCO heritage in Slovakia. Romantic castle with Gothic and Renaissance elements built in 12th century.11637d2f.png"
+  },
+  {
+    "revision": "8f94f235704f1c4145430e9b4e6034c3",
+    "url": "/static/media/DJI_0002.8f94f235.png"
+  },
+  {
+    "revision": "6f3d86491db4f6368b1ce43faa2a9a24",
+    "url": "/static/media/cart_bag.6f3d8649.svg"
+  },
+  {
+    "revision": "05096ed331e01e50de56aed96e679c4e",
+    "url": "/static/media/close.05096ed3.svg"
+  },
+  {
+    "revision": "a2f1c670e1e6c65616572315e6955a08",
+    "url": "/static/media/logo.a2f1c670.svg"
+  },
+  {
+    "revision": "5ba3a8ca97767f6ff3e1be94dff21b4b",
+    "url": "/static/media/lupa.5ba3a8ca.svg"
+  },
+  {
+    "revision": "9efe1bb4645f061f44127ff46a792778",
+    "url": "/static/media/row.9efe1bb4.svg"
+  },
+  {
+    "revision": "300e1872e86787c85de85bb4f2ba7155",
+    "url": "/static/media/row2.300e1872.svg"
+  },
+  {
+    "revision": "e33e818b858b534dd913abd7a7eff5c4",
+    "url": "/static/media/sunset time photon image mid-summer.e33e818b.png"
+  }
+]);
