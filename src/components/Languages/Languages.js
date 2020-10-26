2bccdebac8d5ba29a23066777fe805b097480fc5
Edwardffigueroa <edwardfigueroavelasco@gmail.com>
2020-10-26 10:30:18 -0500

    languajes button fixes #10

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './Languages.module.css'
 
 const Languages = (props) => {
 
-	const changeLanguage = e => console.log(e.target)
+	const [t, seti18n] = useState('fr');
+	const changeLanguage = e => seti18n(e.target.textContent)
+
+	// const changeLanguage = e => console.log(e.target)
 
 	const style = props.capitalize
 		? [classes.Option, classes.Capitalize]
 		: [classes.Option, classes.Uppercase]
 
-	const options = props.lang.map(lang => <span key={lang} onClick={changeLanguage} className={style.join(' ')}>{lang}</span>)
+	// const options = props.lang.map(lang => <span key={lang} onClick={changeLanguage} className={style.join(' ')}>{lang}</span>)
+	const options = props.lang.map(lang => <span key={lang} onClick={changeLanguage} className={`${style.join(' ')} ${lang == t ? classes.Selected : ""}`}>{lang}</span>)
+	options.splice(1, 0, <span key={'separator'} className={classes.Separator}>/</span>);
 
 	return (
 		<div className={classes.Languages}>{options}</div>
 	)
+
 }
 
 export default Languages
