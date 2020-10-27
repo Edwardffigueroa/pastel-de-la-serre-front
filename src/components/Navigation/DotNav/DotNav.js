3f252d158b4b86d7b1801289ea8502e51ff633f8
JuanSevillano <sevi0_6@hotmail.com>
2020-10-27 04:34:07 +0100

    number into active dot added, +1px in size and +10px in left positioning. fixes #9

--- src/components/Navigation/DotNav/DotNav.js
+++ src/components/Navigation/DotNav/DotNav.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import classes from './DotNav.module.css'
-import { NavLink, useLocation } from 'react-router-dom'
 
 const DotNav = ({ hide, current, goSectionHandler }) => {
     const isVisible = hide ? [classes.DotNav, classes.Hidden].join(' ') : classes.DotNav
@@ -9,14 +8,17 @@ const DotNav = ({ hide, current, goSectionHandler }) => {
             <span
                 onClick={e => goSectionHandler(0)}
                 className={current === 0 ? [classes.Dot, classes.Active].join(' ') : classes.Dot}>
+                {current === 0 ? '1' : null}
             </span>
             <span
                 onClick={e => goSectionHandler(1)}
                 className={current === 1 ? [classes.Dot, classes.Active].join(' ') : classes.Dot}>
+                {current === 1 ? '2' : null}
             </span>
             <span
                 onClick={e => goSectionHandler(2)}
                 className={current === 2 ? [classes.Dot, classes.Active].join(' ') : classes.Dot}>
+                {current === 2 ? '3' : null}
             </span>
         </div>
     );
