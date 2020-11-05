49fcecb4d44237bf3a59952a26c87b94c8d5e055
Edwardffigueroa <edwardfigueroavelasco@gmail.com>
2020-11-04 23:00:09 -0500

    booking

--- src/containers/Booking/Booking.js
+++ src/containers/Booking/Booking.js
@@ -5,7 +5,7 @@ import classes from './Booking.module.css'
 const Booking = (props) => {
 	return (
 		<div className="reserv">
-			<div class="easyloisirs_module" data-hash="44c0b316a69a286fb40cf81166f64328d4353cb0"></div>
+			<div className="easyloisirs_module" data-hash="44c0b316a69a286fb40cf81166f64328d4353cb0"></div>
 		</div>
 	)
 }
